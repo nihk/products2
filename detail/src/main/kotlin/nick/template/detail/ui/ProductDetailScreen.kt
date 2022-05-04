a147a281924f18b871817fff866ed2f9789b2b0c
Nick Rose <nickjrose@gmail.com>
2022-05-03 20:23:49 -0400

    Use different configurations for detail screen

--- detail/src/main/kotlin/nick/template/detail/ui/ProductDetailScreen.kt
+++ detail/src/main/kotlin/nick/template/detail/ui/ProductDetailScreen.kt
@@ -1,9 +1,11 @@
 package nick.template.detail.ui
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,6 +17,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -25,6 +28,7 @@ import coil.ImageLoader
 import coil.compose.AsyncImage
 import javax.inject.Inject
 import nick.template.detail.R
+import nick.template.detail.models.ProductDetailItem
 import nick.template.navigation.Screen
 
 internal class ProductDetailScreen @Inject constructor(
@@ -45,44 +49,18 @@ internal class ProductDetailScreen @Inject constructor(
         val product = state.product
 
         if (product != null) {
-            // fixme: landscape is getting cut off and verticalScroll breaks size of image for some reason
-            Column {
-                AsyncImage(
-                    model = product.imageUrl,
-                    contentDescription = product.name,
-                    imageLoader = imageLoader,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    Text(
-                        text = product.name,
-                        style = MaterialTheme.typography.h6,
-                        modifier = Modifier.weight(1f)
-                    )
-                    Text(
-                        text = product.price,
-                        style = MaterialTheme.typography.body1
-                    )
+            val isPortrait = LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT
+
+            if (isPortrait) {
+                Column {
+                    ProductImage(product = product, modifier = Modifier.fillMaxWidth())
+                    ProductDescription(product = product)
                 }
-                Text(
-                    text =  stringResource(
-                        id = R.string.product_type_formatter,
-                        product.type
-                    ),
-                    style = MaterialTheme.typography.body1,
-                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)
-                )
-                Text(
-                    text =  stringResource(
-                        id = R.string.product_code_formatter,
-                        product.id
-                    ),
-                    style = MaterialTheme.typography.body1,
-                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)
-                )
+            } else {
+                Row {
+                    ProductDescription(product = product, modifier = Modifier.weight(1f))
+                    ProductImage(product = product, modifier = Modifier.fillMaxHeight())
+                }
             }
         } else {
             Box(modifier = Modifier.fillMaxSize()) {
@@ -90,6 +68,58 @@ internal class ProductDetailScreen @Inject constructor(
                     modifier = Modifier.align(Alignment.Center)
                 )
             }
+        }
+    }
+
+    @Composable
+    private fun ProductImage(
+        product: ProductDetailItem,
+        modifier: Modifier = Modifier
+    ) {
+        AsyncImage(
+            model = product.imageUrl,
+            contentDescription = product.name,
+            imageLoader = imageLoader,
+            modifier = modifier
+        )
+    }
+
+    @Composable
+    private fun ProductDescription(
+        product: ProductDetailItem,
+        modifier: Modifier = Modifier
+    ) {
+        Column(modifier = modifier) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = product.name,
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    text = product.price,
+                    style = MaterialTheme.typography.body1
+                )
+            }
+            Text(
+                text = stringResource(
+                    id = R.string.product_type_formatter,
+                    product.type
+                ),
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)
+            )
+            Text(
+                text = stringResource(
+                    id = R.string.product_code_formatter,
+                    product.id
+                ),
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)
+            )
         }
     }
 
